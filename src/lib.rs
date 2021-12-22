20cb6fb7fc47dd1f82b2857e0cc9e147a8f77fb0
MaxXSoft <x@MaxXSoft.net>
2021-12-22 10:51:14 +0800

    (lib) added type alias 'KeyValueList' and unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,12 @@ pub use iter::*;
 pub use list::*;
 pub use node::*;
 
+/// A [`KeyNodeList`] that uses [`GenericNode<K, V>`] as its node type.
+///
+/// `KeyValueList` can store key-value pairs and organize them in the form
+/// of a doubly-linked list.
+pub type KeyValueList<K, V> = KeyNodeList<K, GenericNode<K, V>>;
+
 /// Gets a mutable reference of the previous pointer of the specific node.
 macro_rules! node_prev_mut {
   ($list:expr, $key:expr) => {
@@ -35,3 +41,34 @@ macro_rules! node_next_mut {
   };
 }
 pub(crate) use node_next_mut;
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test_push_into_iter() {
+    let mut list = KeyValueList::new();
+    for i in 0..10 {
+      list.push_back(i, i).unwrap();
+    }
+    for (i, (k, n)) in list.into_iter().enumerate() {
+      assert_eq!(i as i32, k);
+      assert_eq!(i as i32, n.into_value());
+    }
+  }
+
+  #[test]
+  fn test_push_pop() {
+    let mut list = KeyValueList::new();
+    for i in 0..10 {
+      list.push_back(i, i).unwrap();
+    }
+    let mut cur = 9;
+    while let Some((k, n)) = list.pop_back() {
+      assert_eq!(cur, k);
+      assert_eq!(cur, n.into_value());
+      cur -= 1;
+    }
+  }
+}
