b3bdf92d8e8486ea3a0f97c2477c6c5cabb89847
MaxXSoft <x@MaxXSoft.net>
2021-12-22 15:50:45 +0800

    (lib) updated crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
-//! TODO
+//! Doubly-linked list that stores key-node pairs.
 //!
-//! * This doc.
-//! * Examples.
-//! * More trait impls.
+//! [`KeyNodeList`] is a doubly-linked list, it uses a hash map to maintain
+//! correspondence between keys and nodes, and records the previous key and
+//! the next key of the current node in the node itself. There is no pointer
+//! operations during this process, so `key_node_list` is all implemented in
+//! safe Rust.
+//!
+//! You can complete key lookups, key-node pair updates, key-node pair
+//! deletions and other operations of [`KeyNodeList`] in *O*(1)~ time. You
+//! can also use cursor-based interface to traverse or edit the linked list.
 
 mod cursor;
 mod iter;
