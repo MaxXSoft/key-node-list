880093567a45e8da17f045201ad16fd29052190d
MaxXSoft <x@MaxXSoft.net>
2021-12-22 09:18:03 +0800

    (lib) added macro 'node_prev_mut' and 'node_next_mut'

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! TODO
+//!
+//! * This doc.
+//! * Examples.
+//! * More trait impls.
+
 mod cursor;
 mod iter;
 mod list;
@@ -7,3 +13,25 @@ pub use cursor::*;
 pub use iter::*;
 pub use list::*;
 pub use node::*;
+
+/// Gets a mutable reference of the previous pointer of the specific node.
+macro_rules! node_prev_mut {
+  ($list:expr, $key:expr) => {
+    $list
+      .node_mut($key)
+      .unwrap()
+      .prev_mut::<$crate::node::Token>()
+  };
+}
+pub(crate) use node_prev_mut;
+
+/// Gets a mutable reference of the next pointer of the specific node.
+macro_rules! node_next_mut {
+  ($list:expr, $key:expr) => {
+    $list
+      .node_mut($key)
+      .unwrap()
+      .next_mut::<$crate::node::Token>()
+  };
+}
+pub(crate) use node_next_mut;
